0ff497b2997193e4a699d6fcf1e872004757dfb8
paulomelila <paulovitor22@hotmail.com>
2017-08-01 11:37:22 -0700

    MinMax C Program

--- MinMax.c
+++ MinMax.c
@@ -10,7 +10,7 @@ int main() {
 printf("The MIN value you can store in (SIGNED CHAR) variable is %d\n", SCHAR_MIN);
 printf("The MAX value you can store in (SIGNED CHAR) variable is %d\n", SCHAR_MAX);
 
-printf("The MIN value you can store in (UNSIGNED CHAR) variable is %d\n", UCHAR_MAX);
+printf("The MIN value you can store in (UNSIGNED CHAR) variable is %d\n", UCHAR_MIN);
 printf("The MAX value you can store in (UNSIGNED CHAR) variable is %d\n", UCHAR_MAX);
 
 printf("MAX value of char: %lu\n", sizeof(char));
