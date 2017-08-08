1a9b8f74766cc9047df75de90e71a77497173173
paulomelila <paulovitor22@hotmail.com>
2017-08-07 19:17:14 -0700

    add review questions

---
+++ Assignments/Assignment2/review_questions.c
@@ -0,0 +1,36 @@
+// REVIEW QUESTIONS
+
+/*
+1. The division doesn't show remainders, if both numbers are integers.
+The division shows remainders if any of the numbers have floating points.
+
+2. False is represented by 0.
+   True is represented by any number other than 0.
+
+3. a. Used to finish a statement
+   b. Used to finish declarations
+   c. Used to separate conditions in a for loop.
+    
+4. 
+
+ i=0;
+ while (i<10) {
+ printf("1 is %d\n", i);
+ i++;
+ }
+ 
+5. The expression is true (numeric value: 1) 
+
+6. It will print "water" if T is greater or equal to 32 and less than 212.
+
+7. It would print "yes" because 3 is a non-zero value, which means the expression is true.
+
+8. The for loop doesn't have curly brackets, which means only the first statement is looped, resulting in:
+
+a
+a
+a
+b
+c
+
+*/ 
